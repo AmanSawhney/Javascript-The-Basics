1ce790ce19c298c8ce203a16b74b2f2cac9e7962
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-14 14:27:22 -0400

    Learned the basics of defining and accesing elements of an array plus the .length function

---
+++ arrayPractice/arrayBasics.js
@@ -0,0 +1,22 @@
+//review
+//array = [];
+//objects = {};
+//great for lists
+//untyped
+
+//practice
+var baseballTeams = ['Dodgers', 'Giants', 'Mets', 'Yankees', 'Astros'];
+print(baseballTeams)
+//challedge 
+//add a value to the end of array
+baseballTeams[5] = 'Cardinals'; 
+print(baseballTeams[5]);
+//turn 'Giants' into a different team
+baseballTeams[1] = 'Red Socks'; 
+print(baseballTeams[1]);
+//access the 'Dodgers' value and save it to variable myFavoriteTeam
+var myFavoriteTeam = baseballTeams[2];
+print(myFavoriteTeam);
+//acess the lengeth of the array
+print(baseballTeams.length);//acess the lengeth of the array
+
