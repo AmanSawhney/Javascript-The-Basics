fa66949cdf197a420219b2dcf9ed5e61033aa394
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-19 11:30:21 -0400

    Made the longest word algo give a prettier output

--- algorithms/identifyTheLongestWordInAString.js
+++ algorithms/identifyTheLongestWordInAString.js
@@ -1,11 +1,13 @@
+//Implement an algorithm to determine the largest word in a
 var stringWithWords = "I have words and I like the state Mississippi";
 function findTheLongestWord(str) {
 	var words = str.split(" ");
-	return words.reduce(function(a,b) {
+	var longestWord = words.reduce(function(a,b) {
 		if (b.length > a.length) {
 			a = b;
 		}
 		return a;
 	}, " ")
+	return 'The longest word is ' + longestWord + ', and it has ' + longestWord.length + ' characters'
 }
 print(findTheLongestWord(stringWithWords));
