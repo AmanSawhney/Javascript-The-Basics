d8ffae620a901a0dc47c67fec8fc523214e9c26c
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-19 09:44:14 -0400

    Finished playing with the map mathod

--- functionalProgramming/functionalProgramming-map.js
+++ functionalProgramming/functionalProgramming-map.js
@@ -1,3 +1,8 @@
+//review
+//map
+//higher order function
+//does not delete but transforms the array
+
 //practice
 var testscores = [100,92,77,68,44,10];
 var testfilter = function(score) {
@@ -8,3 +13,13 @@ var pertange = function(score) {
 }
 var testscoreAfterWeighting = testscores.map(testfilter).map(pertange);
 print(testscoreAfterWeighting);
+//challenge
+//change an array of geetings to all upper case words
+//save the first word in the array to a variable
+var greetings = ['hello', 'hi', 'thanks'];
+var shoutArray = greetings.map(function(salutation) {
+	return salutation.toUpperCase();
+})
+print(shoutArray);
+var firstWord = greetings[0];
+print(firstWord);
